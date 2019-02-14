71ed549826c3e89e1e0241ebf367fa7ee749f5db
Vincent Séré Peyrigain <vincent.serepeyrigain@viacesi.fr>
2019-02-14 17:15:24 +0100

    Add files via upload
    implementation tryback.py function on app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 
 import pymongo
 client = pymongo.MongoClient('drunk:27017')
@@ -24,6 +25,23 @@ def get_experience(selected_database):
     for x in collection.find({}, {'experiment.name': 1}).distinct('experiment.name'):
         list.append(x)
     return list
+
+def get_nested(d, keys):
+    if len(keys) == 1:
+        return d[keys[0]]
+    return get_nested(d[keys[0]], keys[1:])
+
+def generate_table_rows(cursor, columns):
+    rows = []
+    for exp in cursor:
+        row = {}
+        for col in columns:
+            row['.'.join(col)] = get_nested(exp, col)
+            rows.append(row)
+    return rows
+
+
+columns = [('_id',), ('status',), ('config', 'dataset', 'name')]
 
 ########################################################################################################################
 # Dashboard Layout / View
@@ -66,8 +84,16 @@ app.layout = html.Div([
             html.Div('Select experiences'),
             html.Div(dcc.Dropdown(id='experience-selector', multi=True))
         ]),
-    ])
+    ]),
 
+    html.Div([
+
+            # Match Table
+            html.Div(
+                dash_table.DataTable(id='table',
+                                     columns=[{"name": '.'.join(col), "id": '.'.join(col)} for col in columns],)
+            )
+    ])
 ])
 
 #######################################################################################################################
@@ -83,6 +109,18 @@ def update_list_experience(selected_database):
     exps = get_experience(selected_database)
     experience_options = ([{'label': exp, 'value': exp} for exp in exps])
     return experience_options
+
+
+@app.callback(Output('table', 'data'),
+              [Input('database-selector', 'value')]
+              [Input('experience-selector', 'value')])
+def update_table(selected_database, selected_experience):
+    db = client[selected_database]
+    collection = db[selected_experience]
+    cursor = collection.find({'experiment.name': str(selected_experience)})
+    rows = generate_table_rows(cursor, columns)
+    return rows
+
 
 
 # start Flask server
