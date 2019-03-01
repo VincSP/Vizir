f161945bfae3f03a16eac7b378b5aad91e82e898
Tom Veniat <tom.veniat@lip6.fr>
2019-03-01 16:03:49 +0100

    Add a datamanager

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import logging
 
 import dash
 
+from back import MongoManager
+
 logger = logging.getLogger('dashvision.{}'.format(__name__))
 
 
-columns = ['_id', 'status']
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+data_manager = MongoManager(server='drunk', port='27017')
+default_columns = ['_id', 'status']
