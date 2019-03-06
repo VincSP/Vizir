6a6797389998f8edf7da82f44f75c9664a846840
Tom Veniat <tom.veniat@lip6.fr>
2019-03-06 11:14:15 +0100

    Restore previous db connection

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config.suppress_callback_exceptions = True
 
-data_manager = MongoManager(server='127.0.0.1', port='27018')
+data_manager = MongoManager(server='drunk', port='27017')
 default_columns = ['_id', 'status']
