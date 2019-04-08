a72970bc6142617613b5154328edeeb983fa47ac
VincSP <vincent.serepeyrigain@viacesi.fr>
2019-04-08 17:36:57 +0200

    Add 'Graphic' tab

---
+++ apps/graphic_viewer.py
@@ -0,0 +1,15 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.graph_objs as go
+
+
+from dash.dependencies import Input, Output, State
+
+from app import app, default_columns, logic_manager
+
+layout = html.Div([
+    html.H3('Graph'),
+    html.Div(id='tab-data', style={'display': 'none'}),
+    html.Div()
+
+])
--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
 from app import app, default_columns, logic_manager
-from apps import config_viewer, datatable
+from apps import config_viewer, datatable, graphic_viewer
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('dashvision.index')
@@ -89,7 +89,8 @@ app.layout = html.Div([
                 value='tab-datatable',
                 children=[
                     dcc.Tab(label='Datatable', value='tab-datatable'),
-                    dcc.Tab(label='Configs', value='tab-config')])],
+                    dcc.Tab(label='Configs', value='tab-config'),
+                    dcc.Tab(label='Graph', value='tab-graph')])],
         className="row"),
     html.Div(id='tab-content', className="row")],
     style={"margin": "2% 3%"})
@@ -201,6 +202,8 @@ def render_content(tab, update_clicks):
         return datatable.layout
     elif tab == 'tab-config':
         return config_viewer.layout
+    elif tab == 'tab-graph':
+        return graphic_viewer.layout
     else:
         return html.Div('Not Found')
 
