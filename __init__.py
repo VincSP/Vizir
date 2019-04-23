fe8c9830083f4edd6786be39f1d2a0a62c63951d
VincSP <vincent.serepeyrigain@viacesi.fr>
2019-04-23 14:12:46 +0200

    add init file

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .apps.config_viewer import *
+from .apps.datatable import *
+from .apps.plot_viewer import *
+from .app.py import *
+from .data import *
+from index import *
+from logic import *
